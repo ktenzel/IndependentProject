22ef70b00d9be289bd3c29c9fffe5194acacaffa
Kyle <ktenzel@gmail.com>
2018-01-12 10:53:32 -0800

    add date and time functionality

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,14 +1,18 @@
 package models;
 import java.util.ArrayList;
+import java.time.LocalDateTime;
 
 public class Event {
     private String content;
     private String description;
     private static ArrayList<Event> instances = new ArrayList<>();
+    private boolean published;
+    private LocalDateTime createdAt;
 
     public Event (String content, String description){
     this.content = content;
     this.description = description;
+    this.createdAt = LocalDateTime.now();
     instances.add(this);
     }
 
@@ -25,5 +29,11 @@ public class Event {
 
     public static void clearAllPosts(){
         instances.clear();
+    }
+    public boolean getPublished(){
+        return this.published;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
     }
 }
--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -3,12 +3,16 @@ package models;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
+import java.time.LocalDateTime;
 import java.security.PublicKey;
 
 import static org.junit.Assert.*;
 
 public class EventTest {
+    public Event setupNewEvent(){
+        return new Event("Welcome", "startevent");
+    }
+
     @Before
     public void setUp() throws Exception {
     }
@@ -23,6 +27,7 @@ public class EventTest {
         Event event = new Event("Welcome", "");
         assertEquals(true, event instanceof Event);
     }
+
     @Test
     public void NewEventGetsContent() throws Exception {
         Event event = new Event("Welcome", "");
@@ -35,19 +40,30 @@ public class EventTest {
         assertEquals("", event.getDescription());
     }
 
-
     @Test
     public void GetsEachNewEvent_true() {
         Event event = new Event("Welcome", "");
         Event otherEvent = new Event("Lunch", "time for lunch");
         assertEquals(2, Event.getAll().size());
     }
+
     @Test
     public void EventArrayContainsAllEvents_true() {
         Event event = new Event("Welcome", "");
         Event otherEvent = new Event("Lunch", "time for lunch");
         assertTrue(Event.getAll().contains(event));
         assertTrue(Event.getAll().contains(otherEvent));
+    }
+
+    @Test
+    public void getPublished_inFalseByDefault_false() throws Exception {
+        Event myEvent = new Event("Welcome", "");
+        assertEquals(false, myEvent.getPublished());
+    }
+    @Test
+    public void getCreatedAt_stampsCurrentTime() throws Exception {
+        Event myEvent = setupNewEvent();
+        assertEquals(LocalDateTime.now().getDayOfWeek(), myEvent.getCreatedAt().getDayOfWeek());
     }
 
 }
